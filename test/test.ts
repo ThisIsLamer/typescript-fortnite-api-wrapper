9b3bf822fc1308d2348a2d88ca09f2f8d9a7f1be
ThisIsLamer <mangushev.maks13@mail.ru>
2021-06-05 21:09:25 +0300

    update test file

--- test/test.ts
+++ test/test.ts
@@ -1,69 +1,74 @@
-import FortniteAPI from "../build/";
+import fortniteAPI from "../src/wrapper";
 
-const fortnite = new FortniteAPI({
+const fortnite = new fortniteAPI({
   apikey: "70993c518ce03d824996b5b7e1ede0f71f0f9e0f",
   language: "ru",
-})
+});
 
-// BrMap
-console.log(fortnite.BrMap());
+(async () =>  {
+  // BrMap
+  console.log(await fortnite.BrMap());
 
-// // Playlists
-// console.log(fortnite.Playlists());
+  // Playlists
+  console.log(await fortnite.Playlists());
 
-// // PlaylistsID
-// console.log(fortnite.PlaylistsId("Playlist_DefaultSolo"));
+  // PlaylistsID
+  console.log(await fortnite.PlaylistsId("Playlist_DefaultSolo"));
 
-// // Banners
-// console.log(fortnite.Banners());
+  // Banners
+  console.log(await fortnite.Banners());
 
-// // BannersColors
-// console.log(fortnite.BannersColors());
+  // BannersColors
+  console.log(await fortnite.BannersColors());
 
-// // BRStats
-// console.log(fortnite.BRStats({ name: "TFue", accountType: "epic", timeWindow: "lifetime", image: "none" }));
+  // BannersColors
+  console.log(await fortnite.BannersColors());
 
-// // BRStatsID
-// console.log(fortnite.BRStatsID("b9afb91df9964891bd49bfe200ae35c7", { accountType: "epic", timeWindow: "lifetime", image: "none" }));
+  // BRStats
+  console.log(await fortnite.BRStats({ name: "TFue", accountType: "epic", timeWindow: "lifetime", image: "none" }));
 
-// // BRShop
-// console.log(fortnite.BRShop());
-
-// // BRShopCombined
-// console.log(fortnite.BRShopCombined());
-
-// // CreatorCode
-// console.log(fortnite.CreatorCode("TFue"));
-
-// // CreatorCodeSearch
-// console.log(fortnite.CreatorCodeSearch("TFue"));
-
-// // CreatorCodeSearchAll
-// console.log(fortnite.CreatorCodeSearchAll("TFue"));
-
-// // CosmeticsNew
-// console.log(fortnite.CosmeticsNew());
-
-// // CosmeticsList
-// console.log(fortnite.CosmeticsList());
-
-// // CosmeticsSearch
-// console.log(fortnite.CosmeticsSearch({ name: "John", matchMethod: "contains", language: "en" }));
-
-// // CosmeticsSearchAll
-// console.log(fortnite.CosmeticsSearchAll({ name: "Gui", matchMethod: "contains", language: "en" }));
-
-// // CosmeticsSearchByIDs
-// console.log(fortnite.CosmeticsSearchByIDs(["CID_416_Athena_Commando_M_AssassinSuit", "CID_398_Athena_Commando_M_TreasureHunterFashion"]));
-
-// // News
-// console.log(fortnite.News());
-
-// // NewsBR
-// console.log(fortnite.NewsBR());
-
-// // NewsSTW
-// console.log(fortnite.NewsSTW());
-
-// // NewsCreative
-// console.log(fortnite.NewsCreative());
+  // BRStatsID
+  console.log(await fortnite.BRStatsID("b9afb91df9964891bd49bfe200ae35c7", { accountType: "epic", timeWindow: "lifetime", image: "none" }));
+  
+  // BRShop
+  console.log(await fortnite.BRShop());
+  
+  // BRShopCombined
+  console.log(await fortnite.BRShopCombined());
+  
+  // CreatorCode
+  console.log(await fortnite.CreatorCode("TFue"));
+  
+  // CreatorCodeSearch
+  console.log(await fortnite.CreatorCodeSearch("TFue"));
+  
+  // CreatorCodeSearchAll
+  console.log(await fortnite.CreatorCodeSearchAll("TFue"));
+  
+  // CosmeticsNew
+  console.log(await fortnite.CosmeticsNew());
+  
+  // CosmeticsList
+  console.log(await fortnite.CosmeticsList());
+  
+  // CosmeticsSearch
+  console.log(await fortnite.CosmeticsSearch({ name: "John", matchMethod: "contains", language: "en" }));
+  
+  // CosmeticsSearchAll
+  console.log(await fortnite.CosmeticsSearchAll({ name: "Gui", matchMethod: "contains", language: "en" }));
+  
+  // CosmeticsSearchByIDs
+  console.log(await fortnite.CosmeticsSearchByIDs(["CID_416_Athena_Commando_M_AssassinSuit", "CID_398_Athena_Commando_M_TreasureHunterFashion"]));
+  
+  // News
+  console.log(await fortnite.News());
+  
+  // NewsBR
+  console.log(await fortnite.NewsBR());
+  
+  // NewsSTW
+  console.log(await fortnite.NewsSTW());
+  
+  // NewsCreative
+  console.log(await fortnite.NewsCreative());
+})();
